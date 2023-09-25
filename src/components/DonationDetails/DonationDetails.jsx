d4c09d9794e2b1c60daa5c4493960637f5ac3934
Armaan-Hossain-Niloy <niloy.barca@outlook.com>
2023-09-26 03:03:07 +0600

    Donation detail part add

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -1,9 +1,21 @@
+import { useLoaderData, useParams } from "react-router-dom";
 
 
 const DonationDetails = () => {
+    const donations = useLoaderData();
+    const {id} = useParams();
+    const idx = parseInt(id);
+    const donation = donations.find(donation => donation.id === idx);
+    const { picture, description, card_bg, title, category_bg, text_button_bg} = donation;
     return (
-        <div>
-            <h2>it donation <details></details></h2>
+        <div className="mx-40 mt-10">
+            <div>
+                <img className="w-full h-1/2" src={picture} alt="" />
+            </div>
+            <div className="mt-10">
+                <h2 className="text-5xl font-extrabold">{title}</h2>
+                <p className="text-base font-medium text-justify mt-10 mb-36">{description}</p>
+            </div>
         </div>
     );
 };
