b3b5ef0cc9bb7f045b13636979298e30c93784b4
Armaan-Hossain-Niloy <niloy.barca@outlook.com>
2023-09-26 16:26:21 +0600

    view btn handle successfully

--- src/components/SaveDonationCard/SaveDonationCard.jsx
+++ src/components/SaveDonationCard/SaveDonationCard.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SaveDonationCard = ({ donation }) => {
     const { picture, category, card_bg, title, category_bg, text_button_bg, id, price } = donation;
     return (
-        <div style={{backgroundColor: card_bg}} className="card lg:card-side bg-base-100 shadow-xl">
+        <div style={{ backgroundColor: card_bg }} className="card lg:card-side bg-base-100 shadow-xl">
             <figure><img src={picture} alt="Album" /></figure>
             <div className="card-body">
-                <button style={{backgroundColor: category_bg}} className='p-2 w-[100px] rounded-md'>{category}</button>
-                <h2 style={{color: text_button_bg}} className="card-title">{title}</h2>
+                <button style={{ backgroundColor: category_bg }} className='p-2 w-[100px] rounded-md'>{category}</button>
+                <h2 style={{ color: text_button_bg }} className="card-title">{title}</h2>
                 <p>${price}</p>
                 <div className="card-actions">
-                    <button style={{backgroundColor: text_button_bg}} className="btn text-white">View Details</button>
+                    <Link to={`/${id}`}>
+                        <button style={{ backgroundColor: text_button_bg }} className="btn text-white">View Details</button>
+                    </Link>
                 </div>
             </div>
         </div>
