8a85cb7aceae91b5749cd99a80796557866ab5df
Armaan-Hossain-Niloy <niloy.barca@outlook.com>
2023-09-26 23:09:06 +0600

    See All btn for donation page, problem solved successfully

--- src/components/SaveDonation/SaveDonation.jsx
+++ src/components/SaveDonation/SaveDonation.jsx
@@ -6,6 +6,11 @@ import SaveDonationCard from "../SaveDonationCard/SaveDonationCard";
 const SaveDonation = () => {
     const donations = useLoaderData();
     const [appliedDonation, setAppliedDonation] = useState([]);
+    const [dataLength, setDataLength] = useState(4);
+
+    const handleDonationFilter = () =>{
+        setDataLength(appliedDonation.length);
+    }
 
     useEffect(() => {
         const storedDonationId = getStoredDonation();
@@ -13,12 +18,17 @@ const SaveDonation = () => {
         setAppliedDonation(donationApplied);
     }, [donations])
 
-    // console.log(appliedDonation);
+
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mx-40">
-            {
-                appliedDonation.map(donation =><SaveDonationCard key={donation.id} donation={donation}></SaveDonationCard>)
-            }
+        <div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mx-40">
+                {
+                    appliedDonation.slice(0, dataLength).map(donation => <SaveDonationCard key={donation.id} donation={donation}></SaveDonationCard>)
+                }
+            </div>
+            <div className={`flex justify-center items-center mt-10 ${dataLength === appliedDonation.length ? 'hidden' : ''}`}>
+                <button onClick={handleDonationFilter} className={`bg-[#009444] text-white text-xl rounded-md p-5 font-bold ${appliedDonation.length > 4 ? '' : 'hidden'}`}>See All</button>
+            </div>
         </div>
     );
 };
